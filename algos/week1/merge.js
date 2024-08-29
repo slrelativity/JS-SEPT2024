900c4d5481b423789ecde97115ce488f4f27bc79
heidi1105 <heidi1105@gmail.com>
2024-08-29 08:54:31 -0600

    Create merge.js

---
+++ algos/week1/merge.js
@@ -0,0 +1,28 @@
+// merge
+const sortedA1 = [];
+const sortedB1 = [2];
+const expectedMerge1 = [2];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [2, 3, 4, 7];
+const sortedB3 = [ 3];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10];
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Efficiently merges two already sorted arrays into a new sorted array.
+ * Do not mutate the given arrays.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ */
+function merge(left = [], right = []) {}
