9a9935db88841d56d54dfdfe7ea69b7ccbda8d3e
heidi1105 <heidi1105@gmail.com>
2024-09-04 09:08:30 -0600

    Update w2d3.js

--- algos/week2/w2d3.js
+++ algos/week2/w2d3.js
@@ -5,13 +5,13 @@
 const riverLevels1 = [15, 17, 30];
 const expected1 = 15; // 30 - 15 = 15
 
-// temp = 20
+// temp = 50-16 = 34
 const riverLevels2 = [15, 17, 30, 16, 50, 17, 25, 9, 3]; 
-const expected2 = 20; // 25-5 = 20
+const expected2 = 34; 
 
 // 
 const riverLevels3 = [21, 18, 10, 11, 14, 9, 5, 13, 15, 7, 1, 6, 12, 4];
-const expected3 = 11; // 12-1 = 11
+const expected3 = 11; // 11
 
 const riverLevels4 = [1, 5];
 const expected4 = 4;
