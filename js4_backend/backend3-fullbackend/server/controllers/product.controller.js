d575752bdfc676ef706abfa2cda3bb1387903066
heidi1105 <heidi1105@gmail.com>
2024-09-06 11:24:16 -0600

    Backend with db

---
+++ js4_backend/backend3-fullbackend/server/controllers/product.controller.js
@@ -0,0 +1,21 @@
+import Product from "../models/product.model.js"
+
+const ProductController = {
+    getAll: async (req, res)=>{
+        res.json("Testing")
+    },
+    getOne: async (req, res)=>{
+
+    },
+    create: async (req, res)=>{
+
+    },
+    update: async (req, res)=>{
+
+    },
+    delete: async (req, res)=>{
+
+    },
+}
+
+export default ProductController
