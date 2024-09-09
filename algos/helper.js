c95d456d2f631231bf62fc6a2f0b739892330895
heidi1105 <heidi1105@gmail.com>
2024-09-09 09:16:42 -0600

    Update helper.js

--- algos/helper.js
+++ algos/helper.js
@@ -4,7 +4,7 @@ const animalList =["cats", "dogs", "pokemons", "unicorn", "dragons"]
 // Object
 const pet ={
   petname: "Pepper",
-  age: 14,
+  age: 16,
   haircolor: "White"
 }
 
@@ -13,9 +13,19 @@ const proto = { eyecolor: "green", protoFunction: "protoValue" };
 const petWithProto = Object.assign(Object.create(proto), pet);
 
 // loop through an array (normal for-loop)
+for(let i = 0 ; i < animalList.length ; i++){
+    console.log(animalList[i]);
+}
 
 // loop through an array (for...of)
-
+for(const eachAnimal of animalList){
+    console.log(eachAnimal);
+}
 
 // loop through an object (for ...in)
+for(const eachKey in petWithProto){
+    console.log(Object.hasOwn(petWithProto, eachKey));
+    console.log(`${eachKey} : ${petWithProto[eachKey]}`);
+}
 
+console.log(Object.entries(petWithProto));
